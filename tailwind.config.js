1ca70cb7b6957ff3355b3d2ef030126564462a29
cascherma <cascherma@wavecommunications.tech>
2023-07-13 20:02:10 -0300

    Change build to docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html", "./build/js/*.js"],
+  content: ["./docs/*.html", "./docs/js/*.js"],
   theme: {
     extend: {
         screens:{
